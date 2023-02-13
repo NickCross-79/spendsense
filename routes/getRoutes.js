dc2db45a042e90abfd6c0370b2c82831c3e754f6
Nick-Cross-79 <ncross7799@gmail.com>
2023-02-13 15:27:45 -0500

    Added get route for total income

--- routes/getRoutes.js
+++ routes/getRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { ObjectId } from 'mongodb';
+import IncomeController from '../controllers/incomeController.js'
 import Budget from '../models/budgetModel.js';
 import Income from '../models/incomeModel.js';
 import Expense from '../models/expenseModel.js';
 
 const router = express.Router();
+router.use(express.json());
 
 //Get all budgets
 router.get('/budget', (req,res) => {
@@ -36,13 +38,23 @@ router.get('/income', (req,res) => {
         })
 })
 
+//Get total income
+router.get('/income/total', (req, res) => {
+    console.log("Get total income");
+    console.log(req.body.userId);
+    IncomeController.totalIncome(req.body.userId).then(total => {
+        console.log(total);
+        res.sendStatus(200);
+    });
+})
+
 //Get an income
 router.get('/income/:id', (req, res) => {
     console.log("Get an income by id");
     Income.findOne({_id: ObjectId(req.params.id)})
         .then(result => {
             console.log(result);
-            res.send(result);
+            res.sendStatus(200);
         });
 });
 

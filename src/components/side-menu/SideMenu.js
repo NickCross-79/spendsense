154227fd30682020e7fac96cfcdce60b1148d6fb
Nick-Cross-79 <ncross7799@gmail.com>
2023-03-07 03:12:43 -0500

    Added button for testing plaid link token creation

--- src/components/side-menu/SideMenu.js
+++ src/components/side-menu/SideMenu.js
@@ -1,16 +1,28 @@
 import AccountBalance from "./AccountBalance";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function importAll(r){
     let images = {};
     r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
     return images;
 }
-
 const images = importAll(require.context('../../icons', false, /\.(png|jpe?g|svg)$/));
 
 const SideMenu = () => {
+
+    const [response, setResponse] = useState(null);
+
+    const handleClick = () => {
+        axios.post('http://localhost:3001/plaid/create_link_token/64056143a3f9df09bb5801a9')
+            .then(res => {
+                console.log(res.data);
+            })
+    }
+
     return ( 
         <div className="side-menu">
+            <button onClick={handleClick}>Test Link Token</button>
             <p>Account</p>
             <img src={images['icon_profile_.png']} alt="Profile" />
             <AccountBalance />

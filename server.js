66cbf0af9235337054c4886e8550fee8adf96a18
Nick-Cross-79 <ncross7799@gmail.com>
2023-03-07 01:48:18 -0500

    Added cors dependancy

--- server.js
+++ server.js
@@ -10,12 +10,7 @@ const app = express();
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(cors());
 app.use(getRoutes);
 app.use(postRoutes);
 app.use(deleteRoutes);
